fa465e4b4b808f7d60fe4d4d9ca3efb99572bf55
marketapolakova <polakovamarketa@email.cz>
2021-05-02 21:05:49 +0200

    bcrypt

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const express=require("express")
 const ejs = require("ejs")
 const app =express()
 const mongoose=require("mongoose")
-// const encrypt = require("mongoose-encryption")
-const md5=require("md5")
+const bcrypt=require("bcryptjs")
+const saltRounds=10
 
 
 app.set("view engine", "ejs")
@@ -19,7 +19,7 @@ const userSchema=new mongoose.Schema({
 })
 
 
-// userSchema.plugin(encrypt, {secret:process.env.SECRET, encryptedFields:["password"]})
+
 
 const User = mongoose.model("User", userSchema)
 
@@ -36,9 +36,10 @@ app.get("/register", function(req,res){
 })
 
 app.post("/register", function(req, res){
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash){
     const newUser=new User({
         email: req.body.username,
-        password:md5(req.body.password)
+        password:hash
     })
     newUser.save(function(err){
         if(err){
@@ -47,23 +48,27 @@ app.post("/register", function(req, res){
             res.render("secrets")
         }
     })
+    })
+
 })
 
 app.post("/login", function(req, res){
    const username= req.body.username
-   const password=md5(req.body.password)
+   const password=req.body.password
 
    User.findOne({email:username}, function(err, foundUser){
        if (err){
            console.log(err);
        }else{
            if(foundUser){
-               if(foundUser.password===password){
-                    res.render("secrets")
-               }else{
-                   res.render("login")
-               
-               }
+               bcrypt.compare(password, foundUser.password, function(err, results){
+                   if(results===true){
+                       res.render("secrets")
+                   }else{
+                       res.render("login")
+                   }
+               })
+
            }else{
             res.render("register")
            }
